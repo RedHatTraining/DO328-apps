16b65167c38cebaea712fd5589ac44a5db0ce42b
Pablo Solar Vilariño <psolarvi@redhat.com>
2020-03-23 17:19:56 +0100

    Typo

--- mczernek-exchange-application/exchange/src/main/java/com/redhat/restclient/NewsService.java
+++ mczernek-exchange-application/exchange/src/main/java/com/redhat/restclient/NewsService.java
@@ -14,6 +14,6 @@ import java.util.List;
 public interface NewsService {
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)q
+    @Produces(MediaType.APPLICATION_JSON)
     List<News> getFinancialNews();
 }
