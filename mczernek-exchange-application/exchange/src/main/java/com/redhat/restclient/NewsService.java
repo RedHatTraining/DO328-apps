f1d78e2bc1abef2610ef2a95fe68e938239bad0a
Pablo Solar Vilariño <psolarvi@redhat.com>
2020-03-23 17:03:43 +0100

    Removed unused annotations

--- mczernek-exchange-application/exchange/src/main/java/com/redhat/restclient/NewsService.java
+++ mczernek-exchange-application/exchange/src/main/java/com/redhat/restclient/NewsService.java
@@ -14,6 +14,6 @@ import java.util.List;
 public interface NewsService {
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)q
     List<News> getFinancialNews();
 }
