abf5bf540baf359e92f677f9d03be1931b155731
Jaime Ramírez <jaime.ram@gmail.com>
2020-04-27 11:20:21 +0200

    Improved currencies python tracing code

--- mczernek-exchange-application/currencies/src/currencies.py
+++ mczernek-exchange-application/currencies/src/currencies.py
@@ -4,23 +4,31 @@ from flask import Flask, jsonify, request, g
 from flask_cors import CORS
 from jaeger_client import Config
 
+
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
-config = Config(
-    service_name='currencies',
-    validate=True,
-    config={
-        'sampler': {
-            'type': 'const',
-            'param': 1,
-        },
-        'logging': True,
-        'propagation': 'b3'
-    }
-)
-tracer = config.initialize_tracer()
+
+def init_jaeger_tracer():
+    """
+    Initializes a tracer
+    Tracer is initialized inside a function to prevent deadlocks:
+    https://github.com/jaegertracing/jaeger-client-python#initialization--configuration
+    """
+    # These configuration parameters can't be read from env variables
+    # so we need to explicitly set them here
+    config = Config(
+        service_name='currencies',
+        validate=True,
+        config={
+            'sampler': {'type': 'const', 'param': 1},
+            'logging': True,
+            'propagation': 'b3'
+        }
+    )
+    return config.initialize_tracer()
 
 
+tracer = init_jaeger_tracer()
 app = Flask(__name__)
 CORS(app)
 
@@ -29,12 +37,11 @@ CORS(app)
 def start_span():
     headers_dict = {key: value for key, value in request.headers.items()}
     context = tracer.extract(ot.Format.HTTP_HEADERS, headers_dict)
-    span_name = f"{request.method}:currencies"
+    span_name = f'{request.method}:currencies'
     scope = tracer.start_active_span(span_name, child_of=context)
     scope.span.set_tag(ot.tags.HTTP_URL, request.url)
     scope.span.set_tag(ot.tags.HTTP_METHOD, request.method)
     g.scope = scope
-
 
 
 @app.after_request
