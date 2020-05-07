a571becddd95f8ec6bfd2e5b32bd600fead3f9a3
Jaime Ramírez <jaime.ramirez@redhat.com>
2020-05-07 11:29:37 +0200

    Added eslint to dashboard backend

--- dashboard/backend/index.js
+++ dashboard/backend/index.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const _const = require('./lib/constants');
-const _helper = require('./lib/helpers')
-const request = require('request');
+const express = require("express");
+const bodyParser = require("body-parser");
+const _const = require("./lib/constants");
+const _helper = require("./lib/helpers");
+const request = require("request");
 
 const app = express();
 
@@ -17,33 +17,33 @@ app.use(function(_, res, next) {
 });
 
 const checkHistory = (req, resp) => {
-    request.post(_const.HISTORY_FQDN, {json: _const.HISTORY_DATA, timeout: 1500}, (error, res, body) => {
-        _helper.handleResponse(resp, error, res)
-    })
-}
+    request.post(_const.HISTORY_FQDN, {json: _const.HISTORY_DATA, timeout: 1500}, (error, res) => {
+        _helper.handleResponse(resp, error, res);
+    });
+};
 
 const checkCurrency = (req, resp) => {
-    request.get(_const.CURRENCY_FQDN, {timeout: 1500}, (error, res, body) => {
-        _helper.handleResponse(resp, error, res)
-    })
-}
+    request.get(_const.CURRENCY_FQDN, {timeout: 1500}, (error, res) => {
+        _helper.handleResponse(resp, error, res);
+    });
+};
 
 const checkExchangeGateway = (req, resp) => {
-    request.get(_const.EXCHANGE_FQDN, {timeout: 1500}, (error, res, body) => {
-        _helper.handleResponse(resp, error, res)
-    })
-}
+    request.get(_const.EXCHANGE_FQDN, {timeout: 1500}, (error, res) => {
+        _helper.handleResponse(resp, error, res);
+    });
+};
 
 const checkFrontEnd = (req, resp) => {
-    request.get(_const.FRONTEND_FQDN, {timeout: 1500}, (error, res, body) => {
-        _helper.handleResponse(resp, error, res)
-    })
-}
+    request.get(_const.FRONTEND_FQDN, {timeout: 1500}, (error, res) => {
+        _helper.handleResponse(resp, error, res);
+    });
+};
 
-app.get('/history', checkHistory)
-app.get('/currencies', checkCurrency)
-app.get('/exchangeGW', checkExchangeGateway)
-app.get('/frontend', checkFrontEnd)
+app.get("/history", checkHistory);
+app.get("/currencies", checkCurrency);
+app.get("/exchangeGW", checkExchangeGateway);
+app.get("/frontend", checkFrontEnd);
 
 app.listen(_const.PORT, () => {
     console.log(
--- dashboard/backend/lib/helpers.js
+++ dashboard/backend/lib/helpers.js
@@ -1,24 +1,24 @@
-'use strict';
+"use strict";
 
 function handleResponse(clientResp, requestError, requestResp) {
     if(requestError) {
-        console.log("Got error:", requestError)
-        clientResp.json({state: "ERR", isAvailable: false}).status(400)
+        console.log("Got error:", requestError);
+        clientResp.json({state: "ERR", isAvailable: false}).status(400);
     } else {
-        const respCode = requestResp.statusCode
-        let stateString = "Down"
-        let isAvailable = false
+        const respCode = requestResp.statusCode;
+        let stateString = "Down";
+        let isAvailable = false;
 
-        console.log("No error; got response:", respCode)
+        console.log("No error; got response:", respCode);
         if(respCode >= 200 && respCode < 300) {
-            stateString = "OK"
-            isAvailable = true
+            stateString = "OK";
+            isAvailable = true;
         }
-        clientResp.json({state: stateString, isAvailable: isAvailable}).status(respCode)
+        clientResp.json({state: stateString, isAvailable: isAvailable}).status(respCode);
         
     }
 }
 
 module.exports = {
     handleResponse
-}
+};
