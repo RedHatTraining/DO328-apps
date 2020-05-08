91b6983b5ceb19b02d41af033c99330eb884d8c1
Jaime Ramírez <jaime.ramirez@redhat.com>
2020-05-08 13:50:02 +0200

    Restored maven-simplest

--- maven-simplest/src/main/java/io/vertx/example/HelloWorldEmbedded.java
+++ maven-simplest/src/main/java/io/vertx/example/HelloWorldEmbedded.java
@@ -1,23 +1,16 @@
 package io.vertx.example;
 
-import java.util.Optional;
+import io.vertx.core.Vertx;
 
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
 public class HelloWorldEmbedded {
 
-    public static void main(String[] args) {
-        String GREETING = Optional.ofNullable(System.getenv("GREETING")).orElse("Hello World!").trim();
-        String MAX_REQUESTS_PER_SECOND = Optional.ofNullable(System.getenv("MAX_REQUESTS_PER_SECOND")).orElse("0").trim();
-
-        if (!MAX_REQUESTS_PER_SECOND.equals("0")) {
-            System.out.println("Service will limit requests to " + MAX_REQUESTS_PER_SECOND + " req/second");
-        }
-
-        HelloServer server = new HelloServer(
-            GREETING,
-            Float.parseFloat(MAX_REQUESTS_PER_SECOND)
-        );
-
-        server.start();
-    }
+  public static void main(String[] args) {
+    // Create an HTTP server which simply returns "Hello World!" to each request.
+    Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World!\n")).listen(8080);
+  }
 
 }
