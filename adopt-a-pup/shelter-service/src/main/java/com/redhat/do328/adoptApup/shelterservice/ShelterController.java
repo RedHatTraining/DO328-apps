eb8f89b8c8485f76b643e0ed1e32de67ddf384bd
jonahkh <jonah@omnitracs.com>
2020-05-13 14:20:27 -0600

    wire getShelterById method

--- adopt-a-pup/shelter-service/src/main/java/com/redhat/do328/adoptApup/shelterservice/ShelterController.java
+++ adopt-a-pup/shelter-service/src/main/java/com/redhat/do328/adoptApup/shelterservice/ShelterController.java
@@ -3,6 +3,7 @@ package com.redhat.do328.adoptApup.shelterservice;
 import com.redhat.do328.adoptApup.shelterservice.model.Shelter;
 import com.redhat.do328.adoptApup.shelterservice.service.ShelterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,5 +27,10 @@ public class ShelterController {
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
     public List<Shelter> getAllShelters() {
         return shelterService.getAllShelters();
+    }
+
+    @RequestMapping(value = "/{shelter-id}/getShelter", method = RequestMethod.GET)
+    public Shelter getShelterById(@PathVariable(value = "shelter-id") String shelterId) {
+        return shelterService.getShelterById(shelterId);
     }
 }
