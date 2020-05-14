60856848391fc557b65fcb6780dfcde4aea5ba9d
jonahkh <jonah@omnitracs.com>
2020-05-14 12:21:43 -0600

    remove unused import

--- adopt-a-pup/adoption-service/src/main/java/com/redhat/do328/adoptApup/adoptionservice/model/Animal.java
+++ adopt-a-pup/adoption-service/src/main/java/com/redhat/do328/adoptApup/adoptionservice/model/Animal.java
@@ -1,6 +1,5 @@
 package com.redhat.do328.adoptApup.adoptionservice.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
