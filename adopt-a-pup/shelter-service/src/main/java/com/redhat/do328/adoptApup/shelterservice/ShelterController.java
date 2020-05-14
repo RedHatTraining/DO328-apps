a924a7e76b2a9a2507918d310a7bf42abe8dd397
jonahkh <jonah@omnitracs.com>
2020-05-14 11:43:39 -0600

    shelter service api patch

--- adopt-a-pup/shelter-service/src/main/java/com/redhat/do328/adoptApup/shelterservice/ShelterController.java
+++ adopt-a-pup/shelter-service/src/main/java/com/redhat/do328/adoptApup/shelterservice/ShelterController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/shelter")
+@RequestMapping("/shelters")
 public class ShelterController {
 
     @Autowired
