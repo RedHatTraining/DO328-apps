8114d1b38e569216ef776bf606b39a1acf2bc1fb
Marek Czernek <mczernek@redhat.com>
2020-05-18 15:50:15 +0200

    Version frontend

---
+++ exchange-application/frontend/v1/src/NavList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    Nav,
+    NavItem,
+    NavList,
+} from '@patternfly/react-core';
+
+import { Link} from "react-router-dom";
+
+
+
+class NavDefaultList extends React.Component {
+    render() {
+        return (
+            <Nav onSelect={this.onSelect} theme="dark">
+                <NavList>
+                    <NavItem id="home" isActive={window.location.pathname.endsWith("/")}>
+                        <Link to="/">Home</Link>
+                    </NavItem>
+                    <NavItem id="historical_Data" isActive={window.location.pathname.endsWith("/history")}>
+                        <Link to="/history" >Historical Data</Link>
+                    </NavItem>
+                    <NavItem id="exchange" isActive={window.location.pathname.endsWith("/exchange")}>
+                        <Link to="/exchange" >Exchange</Link>
+                    </NavItem>
+                </NavList>
+            </Nav>
+        );
+    }
+}
+
+export default NavDefaultList;
