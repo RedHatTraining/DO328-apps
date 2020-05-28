45cebe6fbdc917e03939bbea33322720f2a8a855
Jonah Howard <johoward@localhost.localdomain>
2020-05-28 16:14:15 -0600

    add swagger to shelter service

--- adopt-a-pup/shelter-service/src/main/java/com/redhat/do328/adoptApup/shelterservice/ShelterServiceApplication.java
+++ adopt-a-pup/shelter-service/src/main/java/com/redhat/do328/adoptApup/shelterservice/ShelterServiceApplication.java
@@ -3,9 +3,11 @@ package com.redhat.do328.adoptApup.shelterservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ShelterServiceApplication {
 
 	public static void main(String[] args) {
