435461d2566110a82c4fb636de6657a253716a89
Jaime Ramírez <jaime.ramirez@redhat.com>
2020-06-03 13:10:00 +0200

    Add style change

--- adopt-a-pup/web-app/src/index.tsx
+++ adopt-a-pup/web-app/src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "@patternfly/react-core/dist/styles/base.css";
-import "./App.css";
 import App from "./App";
+import "./App.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
