5ddca5080ed7e0a2e49d02b52c45d6c3b2230961
Pablo Solar Vilariño <psolarvi@redhat.com>
2020-06-04 16:19:13 +0200

    Added frontend deployment + small fixes

---
+++ adopt-a-pup/scripts/deploy-frontend.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Script to deploy the front end of the comprehensive review
+#
+# Usage:
+#   sh scripts/deploy-frontend.sh [namespace] [adoption-endpoint] [animal-endpoint] [shelter-endpoint] [news-enabled] [news-endpoint]
+#
+#   Parameters:
+#     - namespace: Namespace for the deployment. (Optional)
+#     - adoption-endpoint: Adoption endpoint URL. (Optional)
+#     - animal-endpoint: Animal endpoint URL. (Optional)
+#     - shelter-endpoint: Shelter endpoint URL. (Optional)
+#     - news-enabled: Boolean flag to enable the news section in the front end. (Optional)
+#     - news-endpoint: News endpoint URL. (Optional)
+
+default_ingress_endpoint="http://istio-ingressgateway-istio-system.apps.ocp4.example.com/"
+
+namespace=${1:-"adopt-a-pup"}
+adoption_endpoint=${2:-${default_ingress_endpoint}}
+animal_endpoint=${3:-${default_ingress_endpoint}}
+shelter_endpoint=${4:-${default_ingress_endpoint}}
+news_enabled=${5:-1}
+news_endpoint=${6:-"http://news-comprehensive-review-news.apps.ocp4.example.com"}
+
+oc process -n ${namespace} -f ./kubefiles/frontend-template.yaml \
+  -p REACT_APP_ADOPTION_SERVICE_URL=${adoption_endpoint} \
+  -p REACT_APP_ANIMAL_SERVICE_URL=${animal_endpoint} \
+  -p REACT_APP_SHELTER_SERVICE_URL=${shelter_endpoint} \
+  -p REACT_APP_NEWS_ENABLED=${news_enabled} \
+  -p REACT_APP_NEWS_SERVICE_URL=${news_endpoint} \
+  | oc apply -n {namespace} -f -
--- adopt-a-pup/scripts/deploy-mongo.sh
+++ adopt-a-pup/scripts/deploy-mongo.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-oc process -n adopt-a-pup -f ../kubefiles/mongodb-template.yaml \
-  -p MONGODB_USER=developer \
-  -p MONGODB_PASSWORD=developer \
-  -p MONGODB_DATABASE=adopt-a-pup \
-  -p MONGODB_ADMIN_PASSWORD=redhat \
-  | oc apply -n adopt-a-pup -f -
+# Script to deploy MongoDB in a specific namespace
+#
+# Usage:
+#   sh scripts/deploy-mongo.sh [namespace] [user] [password] [database] [admin-password]
+#
+#   Parameters:
+#     - namespace: Namespace for the deployment. (Optional)
+#     - user: MongoDB username to be created. (Optional)
+#     - password: Password to be assigned to the new MongoDB user. (Optional)
+#     - database: Database to be created. (Optional)
+#     - admin-password: Password to be assigned to the MongoDB admin user. (Optional)
+
+namespace=${1:-adopt-a-pup}
+mongodb_user=${2:-developer}
+mongodb_password=${3:-developer}
+mongodb_database=${4:-adopt-a-pup}
+mongodb_admin_password=${5:-redhat}
+
+oc process -n ${namespace} -f ./kubefiles/mongodb-template.yaml \
+  -p MONGODB_USER=${mongodb_user} \
+  -p MONGODB_PASSWORD=${mongodb_password} \
+  -p MONGODB_DATABASE=${mongodb_database} \
+  -p MONGODB_ADMIN_PASSWORD=${mongodb_admin_password}
+  | oc apply -n ${namespace} -f -
