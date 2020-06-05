22115a479e46d8d61f6dfb3581759be8ea098baf
Jaime Ramírez <jaime.ramirez@redhat.com>
2020-06-05 13:54:48 +0200

    Added scripts to generate images for v1 and v2

--- adopt-a-pup/web-app/src/index.tsx
+++ adopt-a-pup/web-app/src/index.tsx
@@ -2,8 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "@patternfly/react-core/dist/styles/base.css";
 import App from "./App";
-import "./App.css";
 import * as serviceWorker from "./serviceWorker";
+
+
+// To simulate a version change, we show some changes in the css.
+// One of these two files are loaded based on a build-time env variable.
+//
+// We use a very basic condition interpretable by the React bundler,
+// so that only one of the files is included in the production bundle.
+// https://create-react-app.dev/docs/adding-custom-environment-variables/
+process.env.REACT_APP_VERSION === "v2" ? require("./v2.css") : require("./v1.css");
+
 
 ReactDOM.render(
     <React.StrictMode>
