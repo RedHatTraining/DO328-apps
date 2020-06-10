e17c563ee2c48f14805c7bf764b26b14af6b0a09
Pablo Solar Vilariño <psolarvi@redhat.com>
2020-06-10 12:29:26 +0200

    Removed MongoDB from adoption and notification services

--- adopt-a-pup/adoption-service/src/main/java/com/redhat/do328/adoptApup/adoptionservice/model/Animal.java
+++ adopt-a-pup/adoption-service/src/main/java/com/redhat/do328/adoptApup/adoptionservice/model/Animal.java
@@ -3,15 +3,11 @@ package com.redhat.do328.adoptApup.adoptionservice.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Document(collection = "animals")
 public class Animal {
-    @Id
     private String animalId;
     private String animalName;
     private String shelterId;
