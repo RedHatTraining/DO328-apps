93fd022df18c9704b255d20f79a1ead7280ba4dc
Pablo Solar Vilariño <60100327+psolarvi@users.noreply.github.com>
2020-06-10 17:29:14 +0200

    Added MongoDB to Service Mesh (#43)

--- adopt-a-pup/scripts/populate-mongo.sh
+++ adopt-a-pup/scripts/populate-mongo.sh
@@ -11,8 +11,9 @@
 kubernetes_app_tag=${1:-mongodb}
 
 mongo_pod=`oc get pod -l app=${kubernetes_app_tag} -o jsonpath='{.items[0].metadata.name}'`
+mongo_container=${kubernetes_app_tag}
 
 echo "Using mongo pod: ${mongo_pod}"
 
-oc exec -i "${mongo_pod}" -- sh -c 'mongoimport --username=developer --password=developer --collection=animals --db=adopt-a-pup --drop' < ./mongo-data/animals.mongo
-oc exec -i "${mongo_pod}" -- sh -c 'mongoimport --username=developer --password=developer --collection=shelters --db=adopt-a-pup --drop' < ./mongo-data/shelters.mongo
+oc exec -i "${mongo_pod}" -c "${mongo_container}" -- sh -c 'mongoimport --username=developer --password=developer --collection=animals --db=adopt-a-pup --drop' < ./mongo-data/animals.mongo
+oc exec -i "${mongo_pod}" -c "${mongo_container}" -- sh -c 'mongoimport --username=developer --password=developer --collection=shelters --db=adopt-a-pup --drop' < ./mongo-data/shelters.mongo
