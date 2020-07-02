5b529fdf9ecb20ab161834c58f83a9e0ecb1633d
Jaime Ramirez <jaime.ramirez@redhat.com>
2020-07-02 10:03:38 +0200

    fix(exchange-app): Catch invalid responses in frontend

--- exchange-application/frontend/v3/src/FetchUtils.js
+++ exchange-application/frontend/v3/src/FetchUtils.js
@@ -1,7 +1,11 @@
 async function fetchWithRetry (url, options=null, retries=3, msTimeout=3000) {
     const timerPromise = createTimerPromise(msTimeout)
     try {
-        return await Promise.race([fetch(url, options), timerPromise])
+        const response = await Promise.race([fetch(url, options), timerPromise]);
+        if (!isValid(response)) {
+            throw new Error(`Received invalid response with status ${response.status}.`);
+        }
+        return response;
     } catch(err) {
         if (retries === 1) throw err;
         console.log(`Retrying due to: ${err}`)
@@ -15,6 +19,10 @@ function createTimerPromise(timeout) {
             reject(new Error('Request Timeout'))
         }, timeout)
     })
+}
+
+function isValid(response) {
+    return response.status >= 200 && response.status < 300;
 }
 
 module.exports = {
