6ad3e04860b3d608e33b6a4ad929cd1c44b4a67b
Jaime Ramirez <jaime.ramirez@redhat.com>
2020-07-02 11:12:29 +0200

    refactor

--- exchange-application/frontend/v3/src/FetchUtils.js
+++ exchange-application/frontend/v3/src/FetchUtils.js
@@ -2,7 +2,7 @@ async function fetchWithRetry (url, options=null, retries=3, msTimeout=3000) {
     const timerPromise = createTimerPromise(msTimeout)
     try {
         const response = await Promise.race([fetch(url, options), timerPromise]);
-        if (!isValid(response)) {
+        if (!response.ok) {
             throw new Error(`Received invalid response with status ${response.status}.`);
         }
         return response;
@@ -19,10 +19,6 @@ function createTimerPromise(timeout) {
             reject(new Error('Request Timeout'))
         }, timeout)
     })
-}
-
-function isValid(response) {
-    return response.status >= 200 && response.status < 300;
 }
 
 module.exports = {
